d2dc5c1c5fa1d5095955a3aa718535f4ba812347
morry48 <yyyyuuuuuuuu0408@gmail.com>
2019-04-03 13:36:45 +0900

    Group:user edit
    グループのユーザーの追加と削除実装

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,5 @@
 $(function() {
   var search_list = $("#user-search-result");
-
   function appendUser(user){
   var html =  `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${ user.name }</p>
@@ -8,33 +7,53 @@ $(function() {
               </div>`
   search_list.append(html);
   };
-  function appendNoUser(usr){
+
+  function appendNoUser(user){
     var html = ` `
   search_list.append(html);
   };
+
+  function buildHTML(id,name){
+    var html = `<div class="chat-group-user clearfix" id="chat-group-user-${id}">
+                 <input name = "chat_group[user_ids][]" type ="hidden" value="${id}"}/>
+                 <p class="chat-group-user__name">${name}</p>
+                 <p class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}">削除</p>
+               </div>`
+    return html
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-
     $.ajax({
       type: 'GET',
       url: '/users',
       data: {name : input},
       dataType: 'json'
     })
-
     .done(function(users){
-      $("#user-search-result").empty();
+      $(".user-search-result").empty();
       if (users.length !==0 && input.length !==0){
         users.forEach(function(user){
           appendUser(user);
         });
       }
       else{
-        appendNoUser("一致するユーザーはいません");
+        appendNoUser("一致するユーザーはいません")
       }
     })
     .fail(function(){
-      console.log('検索に失敗しました');
-    })
+      console.log('検索に失敗しました')
+    });
+  });
+  $("#user-search-result").on('click' ,'.user-search-add',function(){
+      var id = $(this).data('user-id');
+      var name = $(this).data('user-name');
+      var insertHTML = buildHTML(id,name);
+      $('.chat-group-users').append(insertHTML);
+      $(this).parent('.chat-group-user').remove();
+  });
+  $('.chat-group-users').on('click', '.user-search-remove',function(){
+    var id = $(this).data('user-id');
+    $(`#chat-group-user-${id}`).remove();
   });
 });
