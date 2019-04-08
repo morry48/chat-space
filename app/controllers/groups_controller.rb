87b1a8036f4ef6ac48f671653bc121927ba3f544
morry48 <yyyyuuuuuuuu0408@gmail.com>
2019-04-08 11:49:18 +0900

    detailchange

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,8 +8,7 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group=current_user.groups.new(group_params)
-    @group.users << current_user
+    @group=Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -23,7 +22,6 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      @group.users << current_user
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
       render :edit
